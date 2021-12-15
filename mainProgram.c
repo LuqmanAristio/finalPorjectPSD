a0f85152c5cf66d5b2f8a18b92aa106d1fbd49ab
LuqmanAristio <96041357+LuqmanAristio@users.noreply.github.com>
2021-12-15 21:26:24 +0700

    Update mainProgram.c

--- mainProgram.c
+++ mainProgram.c
@@ -182,3 +182,141 @@ void header(){
 	puts("|                         2021                           |");
 	puts("|=========================^^^^===========================|");
 }
+
+void dataMhs(int nimnya){
+	int pil;
+	char rep;
+
+	do{
+	system("cls");
+
+	header();
+	printf("| NIM : %d |\n",nimnya);
+	puts("==========================================================");
+	puts("|                      MAIN MENU                         |");
+	puts("==========================================================");
+	puts(" --> 1. Ubah Data Diri");
+	puts(" --> 2. Cek Kelulusan");
+	puts(" --> 3. Log Out");
+	puts(" --> 4. Exit");
+	puts("==========================================================");
+	printf(" --> Pilihan : ");
+	scanf("%d", &pil);
+
+	switch(pil){
+		case 1 :
+			editDataDiri(nimnya);
+		break;
+		case 2 :
+			cekLulus(nimnya);
+		break;
+		case 3 :
+			login();
+		break;
+		case 4 :
+			exit(1);
+		break;
+		default:
+			puts(" --> Pilihan tidak tersedia");
+		break;
+	}
+
+	puts("==========================================================");
+	printf(" --> Kembali ke menu utama ? (y/t) : ");
+	scanf("%s",&rep);
+
+	}while(rep=='y'||rep=='Y');
+	login();
+}
+
+void editDataDiri(int nimnya){
+	system("cls");
+	struct node *bantu;
+
+	header();
+	puts("|                    EDIT DATA DIRI                       ");
+	puts("==========================================================");
+	bantu=head;
+
+	while(bantu!=NULL){
+		if(bantu->nim==nimnya){
+			break;
+		}
+		bantu=bantu->next;
+	}
+
+		puts(" --> Data Saat Ini :");
+		puts("==========================================================");
+		printf(" --> Nama 		: %s\n",bantu->nama);
+		printf(" --> Jenis Kelamin 	: %s\n",bantu->kelamin);
+		printf(" --> Tanggal Lahir 	: %s\n",bantu->tgl_lahir);
+		printf(" --> Tempat Lahir 	: %s\n",bantu->tempat_lahir);
+		printf(" --> Alamat 		: %s\n",bantu->alamat);
+
+		puts("==========================================================");
+		puts(" --> Input Data Baru");
+		puts("==========================================================");
+		printf(" --> Nama : ");
+		fflush(stdin);
+		gets(bantu->nama);
+		printf(" --> Jenis Kelamin : ");
+		fflush(stdin);
+		gets(bantu->kelamin);
+		printf(" --> Tanggal Lahir : ");
+		fflush(stdin);
+		gets(bantu->tgl_lahir);
+		printf(" --> Tempat Lahir : ");
+		fflush(stdin);
+		gets(bantu->tempat_lahir);
+		printf(" --> Alamat : ");
+		fflush(stdin);
+		gets(bantu->alamat);
+
+		puts("");
+		puts(" --> Data Berhasil Di-Edit");
+}
+
+void cekLulus(int nimnya){
+	struct node *bantu;
+
+	avgGrade();
+
+	puts("==========================================================");
+	puts("|                  INFORMASI KELULUSAN                   |");
+
+
+	bantu=head;
+
+	while(bantu!=NULL){
+		if(bantu->nim==nimnya){
+			if(bantu->grade=='A'||bantu->grade=='B'||bantu->grade=='C'){
+				puts("==========================================================");
+				puts("          *** Selamat Anda Dinyatakan Lulus ***           ");
+				puts("==========================================================");
+				puts(" --> Berikut Informasi Nilai Anda");
+				puts("");
+				printf(" --> UAS		: %.2f\n",bantu->UAS);
+				printf(" --> UTS		: %.2f\n",bantu->UTS);
+				printf(" --> Tugas		: %.2f\n",bantu->tgs);
+				printf(" --> Quiz		: %.2f\n",bantu->quiz);
+				printf(" --> Rata-Rata		: %.2f\n",bantu->rata);
+				printf(" --> Grade		: %c\n",bantu->grade);
+			}
+
+			else{
+				puts("==========================================================");
+				puts("           *** Anda Dinyatakan Tidak Lulus ***            ");
+				puts("==========================================================");
+				puts(" --> Berikut Informasi Nilai Anda");
+				puts("");
+				printf(" --> UAS		: %.2f\n",bantu->UAS);
+				printf(" --> UTS		: %.2f\n",bantu->UTS);
+				printf(" --> Tugas		: %.2f\n",bantu->tgs);
+				printf(" --> Quiz		: %.2f\n",bantu->quiz);
+				printf(" --> Rata-Rata		: %.2f\n",bantu->rata);
+				printf(" --> Grade		: %c\n",bantu->grade);
+			}
+		}
+		bantu=bantu->next;
+	}
+}
